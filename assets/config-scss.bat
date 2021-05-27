8194ddfe76c446dc12e6707e52f703347cf3fd03
Austine <austineroy04@gmail.com>
2021-05-27 16:42:34 +0300

    initial commit

---
+++ assets/config-scss.bat
@@ -0,0 +1,2 @@
+cd C:\xampp\htdocs\Free Tempaltes\fund-raising-charity-website-template\assets
+scss --watch scss/style.scss:css/style.css
---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+      $(document).ready(function(){
+        $('.menu-hamburger').click(function(){
+          $('.menu-hamburger').toggleClass('active')
+          $('.menu').toggleClass('active')
+        });
+      });
+
+$('.counting').each(function () {
+  var $this = $(this),
+    countTo = $this.attr('data-count');
+
+  $({ countNum: $this.text() }).animate({
+    countNum: countTo
+  },
+
+    {
+
+      duration: 3000,
+      easing: 'linear',
+      step: function () {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function () {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    });
+
+});
+
